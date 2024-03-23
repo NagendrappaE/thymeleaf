47a7b6baea43614d932916ce3b2ec0c46457321d
NagendrappaE-evolvus <nagendrappa.e@evolvussolutions.com>
2024-03-23 17:35:00 +0530

    thymeleaf dropdown

--- thymeleaf-layout-boot3/src/main/java/com/ece/thymeleaflayoutboot3/bean/MenuItem.java
+++ thymeleaf-layout-boot3/src/main/java/com/ece/thymeleaflayoutboot3/bean/MenuItem.java
@@ -8,5 +8,9 @@ public class MenuItem {
 	private int id;
 	
 	private String menuName;
+	
+	private String type;
+	
+	private String link;
 
 }
--- thymeleaf-layout-boot3/src/main/java/com/ece/thymeleaflayoutboot3/controller/GlobalController.java
+++ thymeleaf-layout-boot3/src/main/java/com/ece/thymeleaflayoutboot3/controller/GlobalController.java
@@ -3,11 +3,15 @@
  */
 package com.ece.thymeleaflayoutboot3.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ModelAttribute;
 
 import com.ece.thymeleaflayoutboot3.bean.MenuGroup;
+import com.ece.thymeleaflayoutboot3.bean.MenuItem;
 
 /**
  * @author nagendrappae
@@ -16,19 +20,60 @@ import com.ece.thymeleaflayoutboot3.bean.MenuGroup;
 @ControllerAdvice
 public class GlobalController {
 
-	@ModelAttribute(name = "group1")
-	public MenuGroup dashBoard(Model model) {
+	@ModelAttribute(name = "menuGroupList")
+	public List<MenuGroup> group(Model model) {
 
+		List<MenuGroup> groupList = new ArrayList<>();
 		MenuGroup group1 = new MenuGroup();
 
-		group1.setGroupName("InwardMandate");
+		group1.setGroupName("mandate");
 
-		//MenuGroup group2 = new MenuGroup();
+		groupList.add(group1);
 
-		//model.addAttribute("group1", group1);
-		//model.addAttribute("group2", group2);
+		MenuGroup group2 = new MenuGroup();
 
-		return group1;
+		group2.setGroupName("payment");
+
+		groupList.add(group2);
+
+		return groupList;
+
+	}
+
+	@ModelAttribute(name = "menuitemList")
+	public List<MenuItem> menuitem(Model model) {
+
+		List<MenuItem> menuList = new ArrayList<>();
+		MenuItem menu1 = new MenuItem();
+
+		menu1.setMenuName("viewMandate");
+		menu1.setType("INLINE");
+		menu1.setLink("/spomandate/viewmandate");
+		menuList.add(menu1);
+
+		MenuItem menu2 = new MenuItem();
+
+		menu2.setMenuName("editMandate");
+		menu2.setType("INLINE");
+		menu2.setLink("/spomandate/editmandate");
+
+		menuList.add(menu2);
+
+		MenuItem menu3 = new MenuItem();
+
+		menu3.setMenuName("ACK");
+		menu3.setType("QUEUE");
+		menu3.setLink("/spomandate/queueList");
+		menuList.add(menu3);
+
+		MenuItem menu4 = new MenuItem();
+
+		menu4.setMenuName("Pending Send");
+		menu4.setType("QUEUE");
+		menu4.setLink("/spomandate/queueList");
+		menuList.add(menu4);
+
+		return menuList;
 
 	}
 
