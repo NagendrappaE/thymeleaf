9b962713715f1bd1153f17d7b183f54b61acb4bf
NagendrappaE-evolvus <nagendrappa.e@evolvussolutions.com>
2024-03-23 09:01:46 +0530

    thymeleaf layout

---
+++ thymeleaf-layout-boot3/src/main/java/com/ece/thymeleaflayoutboot3/ThymeleafLayoutBoot3Application.java
@@ -0,0 +1,13 @@
+package com.ece.thymeleaflayoutboot3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThymeleafLayoutBoot3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThymeleafLayoutBoot3Application.class, args);
+	}
+
+}
---
+++ thymeleaf-layout-boot3/src/test/java/com/ece/thymeleaflayoutboot3/ThymeleafLayoutBoot3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ece.thymeleaflayoutboot3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThymeleafLayoutBoot3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
